0cc227978e195a89aaca5bf228e1c5fba644834e
Javier G. Leal <jgleal@gmail.com>
2015-10-30 13:03:47 +0100

    actualizado GB

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ var nAplicacion ="Fuentes y Manantiales de Andalucía";
 var urlMapea = "http://mapea-sigc.juntadeandalucia.es";
 var contextos = "wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Mapa,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Ortofoto";
 var layers = "WMS*Fuentes*http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/conocetusfuentes/wms?*fuentesymanantiales*true*false";
-var urlGeosearch = "http://10.240.203.96:8080/geosearch_taller/fuentesymanantiales/search_html";
+var urlGeosearch = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/fuentesymanantiales/search_html";
 var extra = "geosearchbylocation=" + urlGeosearch + "*2000";
 /******************* Proyecciones *********************/
 var prj25830 ="+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs";
